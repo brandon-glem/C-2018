1c31eeb3e209739d9d44b31bc862d10ce5a8e921
brandon-glem <31105149+brandon-glem@users.noreply.github.com>
2018-11-22 09:42:45 -0500

    templades

---
+++ templades/include/PointArray.h
@@ -0,0 +1,29 @@
+#ifndef POINTARRAY_H
+#define POINTARRAY_H
+
+#include "punto.h"
+template<typename TP>
+
+class PointArray
+{
+    public:
+
+        int size;
+        punto<TP> *puntos;
+
+        PointArray();
+        PointArray(const punto<TP> pts[], const int size);
+        PointArray(PointArray &o);
+        ~PointArray();
+
+        void print();
+        int getSize();
+
+        void push_back(const punto<TP> &p);
+        void resize(int newSize);
+
+        void insert(const int pos, const punto<TP> &p);
+        void remove(const int pos);
+};
+
+#endif // POINTARRAY_H
---
+++ templades/src/PointArray.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "PointArray.h"
+
+using namespace std;
+
+template<typename TP>
+PointArray<TP>::PointArray()
+{
+    this->size = 0;
+    this->puntos = new punto<TP>[size];
+}
+
+template<typename TP>
+PointArray<TP>::PointArray(const punto<TP> pts[], const int size) {
+    this->size = size;
+    this->puntos = new punto<TP>[size];
+    for(int i = 0; i < size; i++)
+        puntos[i] = pts[i];
+}
+
+template<typename TP>
+PointArray<TP>::PointArray(PointArray &o){
+    this->size = o.size;
+    this->puntos = new punto<TP>[size];
+    for(int i = 0; i < size; i++)
+        puntos[i] = o.puntos[i];
+}
+
+template<typename TP>
+int PointArray<TP>::getSize(){
+    return size;
+}
+
+template<typename TP>
+PointArray<TP>::~PointArray()
+{
+    delete[] puntos;
+}
+
+template<typename TP>
+void PointArray<TP>::print(){
+    for(int t=0;t<size;t++)
+        puntos[t].print();
+}
+
+template<typename TP>
+void PointArray<TP>::resize(int newSize){
+    punto<TP> *pts = new punto<TP>[newSize];
+    int minsize = (newSize > size)? size: newSize;
+    for(int t=0;t<minsize;t++)
+        pts[t] = puntos[t];
+    delete [] puntos;
+    size = newSize;
+    puntos = pts;
+}
+
+template<typename TP>
+void PointArray<TP>::push_back(const punto<TP> &p){
+    resize(size+1);
+    puntos[size-1] = p;
+}
+
+
+template<typename TP>
+void PointArray<TP>::insert(const int pos,const punto<TP> &p){
+    resize(size+1);
+    for(int y=(size-1);pos-1<y;y--)
+        puntos[y]=puntos[y-1];
+    puntos[pos-1]=p;
+}
+
+template<typename TP>
+void PointArray<TP>::remove(const int pos){
+    for(int y=pos-1;y<size-1;y++)
+        puntos[y]=puntos[y+1];
+    size--;
+    resize(size);
+}
